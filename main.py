a0716b8080be51872a6d100f476171efc7bb5a5f
Andrei <aguhagood2012@yandx.ru>
2024-11-08 11:35:17 +0400

    Add code for tg_bot in main script

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, CallbackContext, Application, ApplicationBuilder
 
+# создаем экземпляр Bot
+bot = Bot(token="6994142818:AAFnemVStle9xIqnHFTz-SpIQGh63huIRGQ")
+
+# создаем приложение
+app = ApplicationBuilder().token("6994142818:AAFnemVStle9xIqnHFTz-SpIQGh63huIRGQ").build()
+
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text("Привет! Я ваш бот.")
+
+start_handler = CommandHandler('start', start)
+app.add_handler(start_handler)
+
+# запускаем бота
+app.run_polling()
